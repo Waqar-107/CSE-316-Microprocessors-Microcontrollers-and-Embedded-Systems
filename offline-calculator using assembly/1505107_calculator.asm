41cefd07181c27f93b69345e4097afd5ab20fbfa
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2018-05-07 12:47:23 +0600

    Update 1505107_calculator.asm

--- offline-calculator using assembly/1505107_calculator.asm
+++ offline-calculator using assembly/1505107_calculator.asm
@@ -4,7 +4,10 @@
     WELCOME_MESSAGE DB 'Welcome to Calculator v 1.0.',0AH,0DH,'Press a to add, s to subtract and m to multiply.',0AH,0DH,'$'
     OPTIONS DB 'Operation: $'
     NEWLINE DB 0AH,0DH,'$' 
-    DBG DB 'IN',0AH,0DH,'$' 
+    DBG DB 'IN',0AH,0DH,'$'
+    
+    PR1 DB 'ENTER FIRST NUMBER: $'
+    PR2 DB 'ENTER SECOND NUMBER: $' 
     
     ANS_HIGH_BIT DW 0
     ANS_LOW_BIT DW 0
@@ -25,22 +28,32 @@
     INPUT_T_HIGH_BIT DW 0
     INPUT_T_LOW_BIT DW 0
     
-    FLAG DW ?
+    FLAG DW ?      ;FLAG FOR MINUS
+    FLAG2 DW ?
     
-    A_MINUS DB 0
-    B_MINUS DB 0
+    A_MINUS DW 0
+    B_MINUS DW 0
     
-    A_EXP DB 0
-    B_EXP DB 0
-    T_EXP DB 0
+    A_EXP DW 0
+    B_EXP DW 0
+    T_EXP DW 0
     
     QUOTIENT_HIGH DW ?
     QUOTIENT_LOW DW ?
     REMAINDER DW ?
     
+    OUT_LOW DW 0
+    OUT_HIGH DW 0 
+    
+    COUNT DW 0 
+    OPT DB 0
+    
+    
 .CODE
 
 ;-------------------------------------------------------------------------
+;SET THE 4 REGISTERS AND CALL
+;THE ANSWER WILL BE FOUND IN ANS_HIGH_BIT AND ANS_LOW_BIT 
 ADDITION PROC
     ;LET BX:AX HIGH:LOW BITS
     ;    DX:CX WILL BE ADDED WITH BX,AX 
@@ -71,6 +84,8 @@ ADDITION ENDP
 
 
 ;-------------------------------------------------------------------------
+;SET INPUT_A/B_HIGH/LOW
+;THE ANSWER WILL BE SAVED IN ANS_VARIABLES 
 SUBTRACTION PROC
    ; a-b= a+(2's compliment of b)
    PUSH AX
@@ -88,8 +103,8 @@ SUBTRACTION PROC
    NEG CX
    
    CMP CX,0
-   JE ADD_CARRY_SUB
-   JMP DONT_ADD_CARRY_SUB
+   JE DONT_ADD_CARRY_SUB
+   JMP ADD_CARRY_SUB
    
    ADD_CARRY_SUB:
         ADD DX,1
@@ -234,6 +249,7 @@ DIVISON ENDP
 
 
 ;-------------------------------------------------------------------------
+;CALL INPUT, IT WILL STORE THE NUMBER IN INPUT_T_HIGH,LOW
 INPUT PROC
     PUSH AX
     PUSH BX
@@ -246,6 +262,7 @@ INPUT PROC
     
     MOV T_EXP,0
     MOV FLAG,0  ;NOT NEGATIVE
+    MOV FLAG2,0 ;FLOATING POINT NOT FOUND, WHEN FOUND WE DO INC T_EXP++
     MOV AH,1    ;FOR '-'
     INT 21H
     
@@ -276,18 +293,28 @@ INPUT PROC
         CMP AL,0AH
         JE  RESTORE_INPUT
         CMP AL,0DH
-        JE  RESTORE_INPUT
+        JE  RESTORE_INPUT 
         
-        SUB AL,'0'
+        ;IS IT .
+        CMP AL,'.'
+        JE SET_FLAG_OP
+        JMP NORMAL_OP
         
-        ;THERE IS A DIGIT FOUND, TAKE IT AND MULTIPLY IT
+        SET_FLAG_OP:
+            MOV FLAG2,1
+            JMP WHILE_INPUT
+        
+        NORMAL_OP:
+        SUB AL,'0' 
+        
+        ;THERE IS A DIGIT FOUND, TAKE IT AND ADD IT WITH TOTAL*10
         
         MOV INPUT_B_LOW_BIT,10
         MOV INPUT_B_HIGH_BIT,0
         MOV BX,INPUT_T_LOW_BIT
-        MOV INPUT_B_LOW_BIT,BX
+        MOV INPUT_A_LOW_BIT,BX
         MOV BX,INPUT_T_HIGH_BIT
-        MOV INPUT_B_HIGH_BIT,BX
+        MOV INPUT_A_HIGH_BIT,BX
         
         ;NOW MULTILY
         CALL MULTIPLICATION
@@ -297,6 +324,26 @@ INPUT PROC
         MOV INPUT_T_HIGH_BIT,BX
         MOV BX,MUL_LOW_BIT
         MOV INPUT_T_LOW_BIT,BX
+        
+        ;NOW ADD INPUT_T WITH THE DIGIT
+        MOV DX,0 
+        MOV AH,0
+        MOV CX,AX
+        
+        MOV BX,INPUT_T_HIGH_BIT
+        MOV AX,INPUT_T_LOW_BIT
+
+        CALL ADDITION
+        
+        ;NOW ANS VARS ARE THE NEW TOTAL
+        MOV BX,ANS_HIGH_BIT
+        MOV INPUT_T_HIGH_BIT,BX
+        MOV BX,ANS_LOW_BIT
+        MOV INPUT_T_LOW_BIT,BX
+        
+        CMP FLAG2,1
+        JNE WHILE_INPUT
+        INC T_EXP
         
         JMP WHILE_INPUT
     
@@ -309,6 +356,71 @@ RET
 INPUT ENDP
 ;-------------------------------------------------------------------------
 
+;-------------------------------------------------------------------------
+;SET OUT_HIGH AND LOW, AND CALL
+OUTPUT PROC
+    PUSH AX
+    PUSH BX
+    PUSH CX
+    PUSH DX
+    
+    ;COUNT VARIABLE COUNTS DIGITS
+    MOV COUNT,0
+ 
+    REPEAT_OP:
+        ;DX:AX,CX
+        ;DX:AX=Q, BX=REM
+        MOV DX,OUT_HIGH
+        MOV AX,OUT_LOW
+        MOV CX,10
+        
+        CALL DIVISON
+        
+        ;NOW THE VARIABLES HAS THE ANSWERS
+        ;SO WE UPDATE THEM
+        MOV BX,QUOTIENT_HIGH
+        MOV OUT_HIGH,BX
+        MOV BX,QUOTIENT_LOW
+        MOV OUT_LOW,BX
+        
+        MOV DX,REMAINDER
+        PUSH DX
+        INC COUNT
+        
+        ;COMPARE QUOTIENT HIGH AND LOW
+        CMP QUOTIENT_LOW,0
+        JNE REPEAT_OP
+        CMP QUOTIENT_HIGH,0
+        JE  READY_OUPUT_MODE
+        JMP REPEAT_OP
+    
+    
+    READY_OUPUT_MODE:
+        MOV CX,COUNT
+        MOV AH,2    
+    SHOW_OUTPUT:
+        POP DX
+        MOV BX,DX
+         
+        MOV DL,BL
+        ADD DL,'0'
+        INT 21H 
+        LOOP SHOW_OUTPUT
+    
+    ;NEWINE
+    MOV DL,0AH
+    INT 21H
+    MOV DL,0DH
+    INT 21H
+    
+    RESTORE_OUTPUT:
+    POP DX
+    POP CX
+    POP BX
+    POP AX
+RET
+OUTPUT ENDP    
+;-------------------------------------------------------------------------
     
 MAIN PROC
               
@@ -321,8 +433,111 @@ MAIN PROC
     MOV AH,9
     INT 21H 
     
-    
-   
+    WHILE_MAIN:
+        LEA DX,OPTIONS
+        MOV AH,9
+        INT 21H
+        
+        MOV AH,1
+        INT 21H
+        
+        ;OPT HAS THE OPTION, a,s, or m
+        MOV OPT,AL
+        
+        ;NEWLINE
+        LEA DX,NEWLINE
+        MOV AH,9
+        INT 21H
+        
+        ;ENTER FIRST NUMBER
+        LEA DX,PR1
+        MOV AH,9
+        INT 21H
+        
+        ;CALL INPUT
+        ;INPUT NUMBER WILL BE IN INPUT_T
+        ;IF NEGATIVE, FLAG=1
+        CALL INPUT
+        MOV BX,INPUT_T_HIGH_BIT
+        MOV BP_INPUT_A_HIGH_BIT,BX
+        MOV BX,INPUT_T_LOW_BIT
+        MOV BP_INPUT_A_LOW_BIT,BX
+        MOV BX,FLAG
+        MOV A_MINUS,BX
+        MOV BX,T_EXP 
+        MOV A_EXP,BX
+        
+        ;NEWLINE
+        LEA DX,NEWLINE
+        MOV AH,9
+        INT 21H
+        
+        ;ENTER FIRST NUMBER
+        LEA DX,PR2
+        MOV AH,9
+        INT 21H
+        
+        ;CALL INPUT
+        ;INPUT NUMBER WILL BE IN INPUT_T
+        ;IF NEGATIVE, FLAG=1
+        CALL INPUT
+        MOV BX,INPUT_T_HIGH_BIT
+        MOV BP_INPUT_B_HIGH_BIT,BX
+        MOV BX,INPUT_T_LOW_BIT
+        MOV BP_INPUT_B_LOW_BIT,BX
+        MOV BX,FLAG
+        MOV B_MINUS,BX
+        MOV BX,T_EXP 
+        MOV B_EXP,BX
+        
+        ;NEWLINE
+        LEA DX,NEWLINE
+        MOV AH,9
+        INT 21H
+        
+        ;NEGATE IF NECESSARY
+        
+        
+        ;FINAL CALCULATIONS
+        ;FIRST FIX THE EXP, TAKE THE DIFFERENCE OF EXP
+        ;THEN MULTIPLY POW(10,DIFF) WITH THE ONE WITH LESS EXP
+        MOV AX,A_EXP
+        
+        CMP AX,B_EXP
+        JG EXP_A_g_B
+        CMP AX,B_EXP
+        JL EXP_B_g_A
+        
+        EXP_EQU:
+        JMP FINAL_CALC  
+        
+        EXP_A_g_B:
+        JMP FINAL_CALC
+        
+        EXP_B_g_A:    
+        JMP FINAL_CALC
+        
+        FINAL_CALC:
+        
+        MOV AL,OPT
+        CMP AL,'a'
+        JE ADD_NUM
+        CMP AL,'s'
+        JE SUB_NUM
+        JMP MUL_NUM
+        
+        ADD_NUM:
+        ;SAVE IN 4 REGISTERS AND GET THE ANSWERS IN ANS
+        BP_INPUT_A_HIGH_BIT DW 0
+        BP_INPUT_A_LOW_BIT DW 0
+        BP_INPUT_B_HIGH_BIT DW 0
+        BP_INPUT_B_LOW_BIT DW 0 
+        JMP WHILE_MAIN
+        
+        SUB_NUM:
+        JMP WHILE_MAIN
+        MUL_NUM:
+        JMP WHILE_MAIN   
     EXIT:
         MOV AH,4CH
         INT 21H
