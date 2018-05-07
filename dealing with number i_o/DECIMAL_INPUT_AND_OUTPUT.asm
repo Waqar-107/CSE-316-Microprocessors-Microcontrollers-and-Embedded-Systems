6858c59eac6b004e21713415134fb14a2bc82dc0
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2018-05-07 10:27:01 +0600

    Update DECIMAL_INPUT_AND_OUTPUT.asm

--- dealing with number i_o/DECIMAL_INPUT_AND_OUTPUT.asm
+++ dealing with number i_o/DECIMAL_INPUT_AND_OUTPUT.asm
@@ -1,117 +1,415 @@
-;FROM DUST I HAVE COME, DUST I WILL BE
-
 .MODEL SMALL
     
-.DATA  
-    MSG1 DB 'Enter Input Array: $' 
-    MSG2 DB 'Sorted Array: $'
-    CNT DW 260 DUP(0) 
-    F DB 0
-    TEMP DW ?
-.CODE  
- 
-READ PROC
-    ;SAVE REGISTERS
-    ;THE NUMBER WILL BE IN AX
+.DATA
+    WELCOME_MESSAGE DB 'Welcome to Calculator v 1.0.',0AH,0DH,'Press a to add, s to subtract and m to multiply.',0AH,0DH,'$'
+    OPTIONS DB 'Operation: $'
+    NEWLINE DB 0AH,0DH,'$' 
+    DBG DB 'IN',0AH,0DH,'$' 
+    
+    ANS_HIGH_BIT DW 0
+    ANS_LOW_BIT DW 0
+    
+    MUL_HIGH_BIT DW 0
+    MUL_LOW_BIT DW 0
+    
+    INPUT_A_HIGH_BIT DW 0
+    INPUT_A_LOW_BIT DW 0
+    INPUT_B_HIGH_BIT DW 0
+    INPUT_B_LOW_BIT DW 0 
+    
+    BP_INPUT_A_HIGH_BIT DW 0
+    BP_INPUT_A_LOW_BIT DW 0
+    BP_INPUT_B_HIGH_BIT DW 0
+    BP_INPUT_B_LOW_BIT DW 0
+    
+    INPUT_T_HIGH_BIT DW 0
+    INPUT_T_LOW_BIT DW 0
+    
+    FLAG DW ?
+    
+    A_MINUS DB 0
+    B_MINUS DB 0
+    
+    A_EXP DB 0
+    B_EXP DB 0
+    T_EXP DB 0
+    
+    QUOTIENT_HIGH DW ?
+    QUOTIENT_LOW DW ?
+    REMAINDER DW ?
+    
+    OUT_LOW DW 0
+    OUT_HIGH DW 0 
+    
+    COUNT DW 0
+    
+.CODE
+
+;-------------------------------------------------------------------------
+ADDITION PROC
+    ;LET BX:AX HIGH:LOW BITS
+    ;    DX:CX WILL BE ADDED WITH BX,AX 
+    PUSH AX
     PUSH BX
     PUSH CX
     PUSH DX
     
-    ;BX HOLDS TOTAL
-    XOR BX,BX
-    XOR AX,AX
+    ADD BX,DX
+    ADD AX,CX
+    JC ADD_CARRY
+    JMP DONT_ADD_CARRY
     
-    REPEAT:
-        ;READ A CHAR
+    ADD_CARRY:
+        ADD BX,1
+    
+    DONT_ADD_CARRY:
+        MOV ANS_HIGH_BIT,BX
+        MOV ANS_LOW_BIT, AX
+        
+        POP DX
+        POP CX
+        POP BX
+        POP AX    
+RET    
+ADDITION ENDP 
+;-------------------------------------------------------------------------
+
+
+;-------------------------------------------------------------------------
+SUBTRACTION PROC
+   ; a-b= a+(2's compliment of b)
+   PUSH AX
+   PUSH BX
+   PUSH CX
+   PUSH DX
+   
+   MOV AX,INPUT_A_LOW_BIT
+   MOV BX,INPUT_A_HIGH_BIT
+   MOV CX,INPUT_B_LOW_BIT
+   MOV DX,INPUT_B_HIGH_BIT
+   
+   ;1'S COMPLIMENT OF B
+   NEG DX
+   NEG CX
+   
+   CMP CX,0
+   JE ADD_CARRY_SUB
+   JMP DONT_ADD_CARRY_SUB
+   
+   ADD_CARRY_SUB:
+        ADD DX,1
+   
+   DONT_ADD_CARRY_SUB:
+   ;NEGATION DONE
+   ;NOW THE VALUES ARE ALREADY SET IN THE REGISTER
+   CALL ADDITION
+   
+   POP DX
+   POP CX
+   POP BX
+   POP AX     
+RET    
+SUBTRACTION ENDP 
+;-------------------------------------------------------------------------   
+ 
+
+;-------------------------------------------------------------------------   
+MULTIPLICATION PROC   
+    
+  PUSH AX
+  PUSH BX
+  PUSH CX
+  PUSH DX 
+  
+  ;A*B IF LSB(B)=1, MULT HAS THE TOTAL, MULT=MULT+A
+  ;FOR BOTH 0 AND 1 LSB, RIGHT SHIFT B, LEFT SHIFT A
+  
+  ;MUL_HIGH_BIT AND MUL_LOW_BIT HAS THE TOTAL
+  ;MOVE THEM TO BX:AX IN EACH ITERATION
+  ;MOV INPUT A IN DX:CX
+  
+  MOV AX,0              ;TOTAL=0
+  MOV MUL_HIGH_BIT,AX
+  MOV MUL_LOW_BIT,AX 
+  
+  WHILE_MULT: 
+  
+      ;CHECK IF HIGH AND LOW BITS OF B ARE 0, OR THEM TO CHECK
+      MOV AX,INPUT_B_LOW_BIT
+      OR AX,AX
+      JNZ CALC_MUL
+      
+      MOV AX,INPUT_B_HIGH_BIT
+      OR AX,AX
+      JZ RESTORE_MULT           ;HIGH IS 0, AND AS LOW=0, IT MADE IT TO THIS INSTRUCTION
+      
+      
+      CALC_MUL:
+      ;CHECK LSB OF B HERE AND SHIFT
+      MOV AX,INPUT_B_LOW_BIT
+      MOV BX,INPUT_B_HIGH_BIT
+      
+      ;SHIFHT HIGH, NOW CF HAS THE LAST BIT OF HIGH
+      SHR BX,1
+      RCR AX,1                  ;B HAS SHIFTED RIGHT, CF HAS THE LSB NOW, CHECK
+      
+      ;SAVE B
+      MOV INPUT_B_LOW_BIT,AX
+      MOV INPUT_B_HIGH_BIT,BX 
+      
+      ;COMPARE CF
+      ;JUMP CARRY, IF CARRY=1, TOTAL+=A; MULT+=A
+      JC UPDATE_TOTAL
+      JMP SHIFT_BIT_A
+      
+      ;CALCULATE
+      UPDATE_TOTAL:
+        MOV BX,MUL_HIGH_BIT
+        MOV AX,MUL_LOW_BIT
+               
+        MOV DX,INPUT_A_HIGH_BIT
+        MOV CX,INPUT_A_LOW_BIT
+      
+        CALL ADDITION
+        
+        ;NOW ANS HAS THE SUM, MOV IT TO MULT
+        MOV AX,ANS_HIGH_BIT
+        MOV MUL_HIGH_BIT,AX
+        
+        MOV AX,ANS_LOW_BIT
+        MOV MUL_LOW_BIT,AX
+        
+      SHIFT_BIT_A:
+        
+        ;NOW MUL HAS THE TOTAL, SO WE CAN LEFT SHIFT A
+        MOV CX,INPUT_A_LOW_BIT
+        MOV DX,INPUT_A_HIGH_BIT
+        
+        SHL CX,1
+        RCL DX,1
+        
+        MOV INPUT_A_LOW_BIT,CX
+        MOV INPUT_A_HIGH_BIT,DX
+        
+        JMP WHILE_MULT
+               
+  RESTORE_MULT:
+      POP DX
+      POP CX
+      POP BX
+      POP AX   
+RET    
+MULTIPLICATION ENDP
+;-------------------------------------------------------------------------
+
+
+;-------------------------------------------------------------------------
+;SAVE A IN BX:AX, SAVE B IN CX
+DIVISON PROC
+    PUSH AX
+    PUSH BX
+    PUSH CX
+    PUSH DX
+    
+    ;DX:AX=DIVINEND
+    ;CX=DIVISOR
+    ;DX:AX=QUOTIENT, BX=REM
+    
+    PUSH AX         ;SAVE LOW WORD
+    MOV AX,DX       ;HIGH WORD TO AX
+    SUB DX,DX       ;SET DX=0
+    DIV CX          ;DIVIDE THE HIGH
+    MOV BX,AX       ;BX=QUOTIENT HIGH WORD
+    POP AX          ;RETRIEVE LOW 
+    DIV CX          ;DIVIDE REMANDER HIGH:DIVINEND LOW
+    XCHG BX,DX      ;SWAPS RESULTS INTO PLACE
+    
+    MOV QUOTIENT_HIGH,DX
+    MOV QUOTIENT_LOW,AX
+    MOV REMAINDER,BX
+    
+    RESTORE_DIV:
+    POP DX
+    POP CX
+    POP BX
+    POP AX
+RET
+DIVISON ENDP
+;-------------------------------------------------------------------------
+
+
+;-------------------------------------------------------------------------
+;CALL INPUT, IT WILL STORE THE NUMBER IN INPUT_T_HIGH,LOW
+INPUT PROC
+    PUSH AX
+    PUSH BX
+    PUSH CX
+    PUSH DX
+    
+    ;SAVE THE INPUT IN BACKUP COPIES, COPY THEM TO NORMALS INPUT-(A,B)-(HIGH,LOW)
+    ;TO WORK
+    ;TOTAL=TOTAL*10, FIRST CHECK FOR MINUS, WHEN THE POINT(.) IS FOUND, START COUNTING
+    
+    MOV T_EXP,0
+    MOV FLAG,0  ;NOT NEGATIVE
+    MOV AH,1    ;FOR '-'
+    INT 21H
+    
+    CMP AL,'-'
+    JNE NOT_SET_MINUS
+    MOV FLAG,1
+    
+    MOV INPUT_T_LOW_BIT,0
+    MOV INPUT_T_HIGH_BIT,0
+    JMP WHILE_INPUT
+    
+    ;IF NOT MINUS, THEN THE FIRST DIGIT IS IN AL 
+    NOT_SET_MINUS:    
+        SUB AL,'0'
+        MOV AH,0;
+        
+        MOV INPUT_T_LOW_BIT,AX
+        MOV INPUT_T_HIGH_BIT,0 
+    
+    WHILE_INPUT:
+        ;KEEP TAKING INPUT TILL WE GET SPACE OR CARRIAGE
         MOV AH,1
         INT 21H
         
-        CMP AL,20H  ;IF SPACE THEN DONE
-        JE RESTORE
-        CMP AL,78H  ;IF X  
-        JE FLAG_CHANGE  
+        ;TERMINATE IF SPACE,CR OR NEWLINE FOUND
+        CMP AL,20H
+        JE  RESTORE_INPUT
+        CMP AL,0AH
+        JE  RESTORE_INPUT
+        CMP AL,0DH
+        JE  RESTORE_INPUT
         
-        ;UPDATE TOTAL=TOTAL*10+DIGIT
-        AND AX,000FH       ;CONVERT AX INTO CHAR->DIGIT
-        MOV CX,AX
+        SUB AL,'0'
         
-        MOV AX,10
-        MUL BX
+        ;THERE IS A DIGIT FOUND, TAKE IT AND MULTIPLY IT
         
-        MOV BX,AX           ;THE PRODUCT IS SAVED IN AX
-        ADD BX,CX
-        JMP REPEAT
+        MOV INPUT_B_LOW_BIT,10
+        MOV INPUT_B_HIGH_BIT,0
+        MOV BX,INPUT_T_LOW_BIT
+        MOV INPUT_B_LOW_BIT,BX
+        MOV BX,INPUT_T_HIGH_BIT
+        MOV INPUT_B_HIGH_BIT,BX
         
-    FLAG_CHANGE:
-        MOV CL,1
-        MOV F,CL
-    RESTORE:   
+        ;NOW MULTILY
+        CALL MULTIPLICATION
+        
+        ;NOW MUL_VARIABLES HAVE THE ANSWERS, MOVE THEM IN TOTAL
+        MOV BX,MUL_HIGH_BIT
+        MOV INPUT_T_HIGH_BIT,BX
+        MOV BX,MUL_LOW_BIT
+        MOV INPUT_T_LOW_BIT,BX
+        
+        JMP WHILE_INPUT
     
-        MOV AX,BX 
-        ;RESTORE REGISTERS
-        POP BX
-        POP CX
+    RESTORE_INPUT:
         POP DX
+        POP CX
+        POP BX
+        POP AX
 RET
-READ ENDP 
+INPUT ENDP
+;-------------------------------------------------------------------------
 
-
-DISPLAY PROC
-    PUSH AX             ;SAVE REGISTERS
+;-------------------------------------------------------------------------
+;SET OUT_HIGH AND LOW, AND CALL
+OUTPUT PROC
+    PUSH AX
     PUSH BX
     PUSH CX
     PUSH DX
     
-    ;THEN 
-    ;PUSH CAUSE AX WILL GET LOST
-    ;WHILE MOV AH,2 INSTRUCTION   
+    ;COUNT VARIABLE COUNTS DIGITS
+    MOV COUNT,0
+ 
+    REPEAT_OP:
+        ;DX:AX,CX
+        ;DX:AX=Q, BX=REM
+        MOV DX,OUT_HIGH
+        MOV AX,OUT_LOW
+        MOV CX,10H
+        
+        CALL DIVISON
+        
+        ;NOW THE VARIABLES HAS THE ANSWERS
+        ;SO WE UPDATE THEM
+        MOV BX,QUOTIENT_HIGH
+        MOV OUT_HIGH,BX
+        MOV BX,QUOTIENT_LOW
+        MOV OUT_LOW,BX
+        
+        MOV DX,REMAINDER
+        PUSH DX
+        INC COUNT
+        
+        ;COMPARE QUOTIENT HIGH AND LOW
+        CMP QUOTIENT_LOW,0
+        JNE REPEAT_OP
+        CMP QUOTIENT_HIGH,0
+        JE  READY_OUPUT_MODE
+        JMP REPEAT_OP
     
-    XOR CX,CX           ;CX COUNTS DIGITS
-    MOV BX,10D          ;BX=10 IN DECIMAL 
-        
-    REPEAT_ON:
-        XOR DX,DX       ;PREPARE HIGH WORD OF DIVINENED
-        DIV BX          ;AX=AX/BX,DX=AX%BX
-        PUSH DX         ;SAVE DX ON STACK
-        INC CX          ;CNT++
-        
-        ;UNTIL
-        OR AX,AX        ;AX==0?
-        JNE REPEAT_ON   ;NO KEEP GOING
-        
-    ;DONE
-    MOV AH,2
-    PRINT_ON:
-        POP DX      ;DIGIT IN DL
+    
+    READY_OUPUT_MODE:
+        MOV CX,COUNT
+        MOV AH,2    
+    SHOW_OUTPUT:
+        POP DX
+        MOV BX,DX
+         
+        MOV DL,BL
         ADD DL,'0'
-        INT 21H
-        LOOP PRINT_ON
+        INT 21H 
+        LOOP SHOW_OUTPUT
     
-    ;RESTORE THE REGISTERS
-    POP AX
-    POP BX
-    POP CX
+    ;NEWINE
+    MOV DL,0AH
+    INT 21H
+    MOV DL,0DH
+    INT 21H
+    
+    RESTORE_OUTPUT:
     POP DX
-
-RET       
-DISPLAY ENDP
-
-MAIN PROC
+    POP CX
+    POP BX
+    POP AX
+RET
+OUTPUT ENDP    
+;-------------------------------------------------------------------------
     
-    ;INIT
+MAIN PROC
+              
+    ;INITIALIZE DATA SEGMENT
     MOV AX,@DATA
     MOV DS,AX
     
+    ;SHOW WELCOME MESSAGE
+    LEA DX,WELCOME_MESSAGE
     MOV AH,9
-    LEA DX,MSG1
+    INT 21H 
+    
+    ;CALL INPUT
+    
+    MOV AH,2
+    MOV DL,0AH
+    INT 21H
+    MOV DL,0DH
     INT 21H
     
-    CALL READ
-    CALL DISPLAY
-
-   
+    ;  
+    MOV OUT_HIGH,2H
+    MOV OUT_LOW,35H 
     
+    CALL OUTPUT
+   
+    EXIT:
+        MOV AH,4CH
+        INT 21H
     
 MAIN ENDP
 
-END MAIN
+END MAIN 
