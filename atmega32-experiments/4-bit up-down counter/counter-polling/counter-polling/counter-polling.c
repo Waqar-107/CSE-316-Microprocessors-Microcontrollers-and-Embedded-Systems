265e7630975e32adb1b9021d5cdc1170c81d179d
Waqar-107 <waqar.hassan866@gmail.com>
2018-05-20 15:39:45 +0600

    updated polling-codes

--- atmega32-experiments/4-bit up-down counter/counter-polling/counter-polling/counter-polling.c
+++ atmega32-experiments/4-bit up-down counter/counter-polling/counter-polling/counter-polling.c
@@ -9,33 +9,36 @@ int main(void)
 	//input in PB4,PB5
 	DDRA=0xFF;
 	DDRB=0x00;
-	DDRC=0xFF;
 	
-	unsigned char ma=0b00001111;
 	unsigned char mb1=0b00010000;
 	unsigned char mb2=0b00100000;
 	
-	unsigned char cnt=0;
+	int cnt=0;
 	
 	while(1)
 	{
-		if(PINB & mb1){
-			cnt++;
-			PORTA=cnt & ma;
-			PORTC=0b00000001;
+		if(PINB & mb1)
+		{
+			if (cnt == 15)
+				cnt = 0;
+			else
+				cnt++;
+			
+			PORTA=cnt;
+		
 			_delay_ms(1000);
 		}
 		
-		else if(PINB & mb2){
-			cnt--;
-			PORTA=cnt & ma;
+		else if(PINB & mb2)
+		{
+			if (cnt == 0)
+				cnt = 15;
+			else
+				cnt--;
+			
+			PORTA=cnt;
+			
 			_delay_ms(1000);
 		}
-		
-		else{
-			PORTC=0;
-		}
-		
-		
 	}
 }
